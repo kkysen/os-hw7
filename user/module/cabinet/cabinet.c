ef72146c5b9d2b8882f4590a18c824f56e164ccd
Khyber Sen <kkysen@gmail.com>
2021-11-29 01:31:35 -0500

    Did most of `inspect_cabinet` but we might be getting some addresses wrong.

--- user/module/cabinet/cabinet.c
+++ user/module/cabinet/cabinet.c
@@ -8,25 +8,70 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/cabinet.h>
-#include <linux/kernel.h>
-#include <linux/sched.h>
 #include <linux/cred.h>
 #include <linux/uaccess.h>
+#include <linux/pgtable.h>
+#include <linux/page_ref.h>
 
 #pragma GCC diagnostic pop
 
 extern long (*inspect_cabinet_ptr)(int pid, unsigned long vaddr,
-		struct cab_info *inventory);
+				   struct cab_info *inventory);
 extern long (*inspect_cabinet_default)(int pid, unsigned long vaddr,
-		struct cab_info *inventory);
+				       struct cab_info *inventory);
 
-void lookup_cab_info(unsigned long vaddr __always_unused, struct cab_info *info) {
-	*info = (struct cab_info) {0};
+void lookup_cab_info(struct task_struct *task, unsigned long vaddr,
+		     struct cab_info *info)
+{
+	pgd_t *pgd;
+	p4d_t *p4d;
+	pud_t *pud;
+	pmd_t *pmd;
+	pte_t *pte;
+	struct page *page;
+
+#define next_offset_using(cur, next, offset, check)                                \
+	do {                                                                       \
+		unsigned long pfn;                                                 \
+		phys_addr_t phys_addr;                                             \
+		next = offset(cur, vaddr);                                         \
+		if (!(check))                                                      \
+			return;                                                    \
+		pfn = next##_pfn(*next);                                           \
+		phys_addr = (phys_addr_t)pfn << PAGE_SHIFT;                        \
+		info->next##_paddr = (unsigned long)phys_addr;                     \
+		/*info->next##_paddr = next##_val(*next);*/                        \
+		/*info->next##_paddr = */                                          \
+			/*(next##_val(*next) & next##_pfn_mask(*next)) +*/         \
+			/*next##_index(vaddr);*/ \
+	} while (false)
+
+#define next_offset(cur, next)                                                 \
+	next_offset_using(cur, next, next##_offset,                            \
+			  !next##_none(*next) &&                               \
+				  !unlikely(next##_bad(*next)) &&              \
+				  next##_present(*next))
+
+	/* zero everything in case we return early */
+	*info = (struct cab_info){ 0 };
+	next_offset(task->mm, pgd);
+	next_offset(pgd, p4d);
+	next_offset(p4d, pud);
+	next_offset(pud, pmd);
+	next_offset_using(pmd, pte, pte_offset_map, pte);
+
+#undef next_offset
+#undef next_offset_using
+
+	info->pf_paddr =
+		(unsigned long)((phys_addr_t)pte_pfn(*pte) << PAGE_SHIFT);
+	info->paddr = pte_val(*pte);
+	info->dirty = pte_dirty(*pte);
+	page = pte_page(*pte);
+	info->refcount = page_ref_count(page);
 }
 
-long inspect_cabinet(int pid,
-		     unsigned long vaddr,
-		     struct cab_info *inventory)
+long inspect_cabinet(int pid, unsigned long vaddr, struct cab_info *inventory)
 {
 	kuid_t euid;
 	bool is_root;
@@ -61,7 +106,7 @@ long inspect_cabinet(int pid,
 
 	/* TODO: check vaddr is a valid addr first? */
 
-	lookup_cab_info(vaddr, &info);
+	lookup_cab_info(task, vaddr, &info);
 	if (copy_to_user(inventory, &info, sizeof(info)) != 0)
 		return -EFAULT;
 
